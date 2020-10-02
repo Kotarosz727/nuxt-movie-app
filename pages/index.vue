2fc0ad400f7b376d1579e60591fc337787d888c7
鈴木光太郎 <suzukikoutarou@suzukikoutarounoMacBook-Pro.local>
2020-10-02 18:30:36 +0900

    一覧画面追加

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,37 @@
 			<h2>上映中</h2>
 		</div>
 		<div class="content_flex">
-			<div class="card" v-for="data in datas" :key="data.id">
+			<div class="card" v-for="data in datas1" :key="data.id">
+				<div class="image">
+						<nuxt-link :to="/movies/ + data.id">
+							<img v-bind:src="imgPreUrl + data.poster_path" alt="img">
+						</nuxt-link>
+				</div>
+				<div class="content">
+					<h3 class="title">{{ data.title }}</h3>
+				</div>
+			</div> 
+		</div>
+		<div class="column_header">
+			<h2>高い評価</h2>
+		</div>
+		<div class="content_flex">
+			<div class="card" v-for="data in datas2" :key="data.id">
+				<div class="image">
+						<nuxt-link :to="/movies/ + data.id">
+							<img v-bind:src="imgPreUrl + data.poster_path" alt="img">
+						</nuxt-link>
+				</div>
+				<div class="content">
+					<h3 class="title">{{ data.title }}</h3>
+				</div>
+			</div> 
+		</div>
+		<div class="column_header">
+			<h2>人気の映画</h2>
+		</div>
+		<div class="content_flex">
+			<div class="card" v-for="data in datas3" :key="data.id">
 				<div class="image">
 						<nuxt-link :to="/movies/ + data.id">
 							<img v-bind:src="imgPreUrl + data.poster_path" alt="img">
@@ -28,7 +58,10 @@
 	.main {
 		padding-top: 50px;
 		padding-left: 70px;
-		max-width: 1300px;
+		max-width: 1400px;
+	}
+	.column_header {
+		padding-top: 40px;
 	}
 	.content_flex {
 		width: 100%;
@@ -47,9 +80,13 @@
 	const axios = require('axios')
 	let base_url = 'https://api.themoviedb.org';
 	let sub_url = '/3/movie/'; 
+	let country_url = '&language=en-US&page=1'
 	let api_key = process.env.MOVIE_API_KEY;
-	let url = base_url+sub_url+'now_playing?'+api_key+'&language=ja'
-	console.log(url);
+	let now_playing_url = base_url + sub_url + 'now_playing?' + api_key + country_url; 
+	let top_rated_url = base_url + sub_url + 'top_rated?' + api_key + country_url;
+	let popular_url = base_url + sub_url + 'popular?' + api_key + country_url; 
+
+	console.log(top_rated_url);
 
 	export default {
 		data: function() {
@@ -61,21 +98,20 @@
 				keyword: ""
             }
         }, 
-		asyncData({params, error}){
-			return axios.get(url)
-			.then((res) => {
-				console.log(res.data.results)
-				return { datas: res.data.results }
-			})
-			.catch((e => {
-			  error({datas: e.response.status_message, message: "Error"})
-			}))
-		},
-		// methods: {
-		// 	submitClick() {
-		// 		this.$router.push("/search?" + this.keyword)
-		// 	}
-		// }
+		async asyncData({ params }) {
+			const [ nowPlayings, topRated, popular] = await Promise.all([
+				axios.get(now_playing_url),
+				axios.get(top_rated_url),
+				axios.get(popular_url)
+			]);
+
+			return {
+				datas1: nowPlayings.data.results,
+				datas2: topRated.data.results,
+				datas3: popular.data.results
+			}
+		} 
+	
 	}
 </script>
 
